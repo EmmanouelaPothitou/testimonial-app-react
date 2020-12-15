d4dcea27184cd72e4df8fe7d0b900c90fc6cee93
Emmanouela Pothitou <emmanouelapothitou@gmail.com>
2020-12-15 15:45:13 +0200

    better handle loading states

--- src/App.js
+++ src/App.js
@@ -1,32 +1,38 @@
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 import "./App.css";
 import { Testimonial } from "./components/Testimonial";
 import { TestimonialGrid } from "./components/TestimonialGrid";
 
 function App() {
+  const [testimonials, setTestimonials] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  
+  useEffect(() => {
+    fetch(`http://localhost:3000/testimonials/`)
+      .then(res => {
+        if (!res.ok) {
+          const error = new Error(res.statusText);
+          error.json = res.json();
+          throw error;
+        }
+        return res.json();
+      })
+      .then(data => setTestimonials(data))
+      .catch(e => setError(e))
+      .finally(() => setIsLoading(false))
+  }, []);
 
-    const [users, setUsers]= useState([]);
-  
-    useEffect(()=> {
-      getTestimonials();
-    }, []);
-  
-  
-    const getTestimonials = async () => {
-      const response = await fetch (`http://localhost:3000/testimonials/`);
-      const data = await response.json();
-      console.log(data);
-      setUsers(data);
-    };
-  
-  const testimonials = users.map((tm) => (
+  const testimonialComponents = testimonials.map((tm) => (
     <Testimonial key={tm.id} {...tm} />
   ));
 
   return (
     <div className="App">
       <div className="Content">
-        <TestimonialGrid>{testimonials}</TestimonialGrid>
+        {error && "An error happened :("}
+        {isLoading && "Loading!"}
+        {!isLoading && !error && <TestimonialGrid>{testimonialComponents}</TestimonialGrid>}
       </div>
     </div>
   );
