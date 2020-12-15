26b07e95c8319aab8057e6113f77871c424f99d7
Emmanouela Pothitou <emmanouelapothitou@gmail.com>
2020-12-15 15:27:24 +0200

    update App homepage component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from 'react'
+import "./App.css";
+import { Testimonial } from "./components/Testimonial";
+import { TestimonialGrid } from "./components/TestimonialGrid";
 
 function App() {
+
+    const [users, setUsers]= useState([]);
+  
+    useEffect(()=> {
+      getTestimonials();
+    }, []);
+  
+  
+    const getTestimonials = async () => {
+      const response = await fetch (`http://localhost:3000/testimonials/`);
+      const data = await response.json();
+      console.log(data);
+      setUsers(data);
+    };
+  
+  const testimonials = users.map((tm) => (
+    <Testimonial key={tm.id} {...tm} />
+  ));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Content">
+        <TestimonialGrid>{testimonials}</TestimonialGrid>
+      </div>
     </div>
   );
 }
